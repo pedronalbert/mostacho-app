9f9ee2eaa82d7c375db2c924d93bb9ed3f1d89e4
Alberto Pedron <pedronalbert@G31T-M7.(none)>
2015-02-22 02:34:49 -0430

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+(function () {
+
+  window.addEventListener('polymer-ready', function () {
+    console.log('index: polymer ready');
+    window.Request = window.superagent;
+    window.BACKEND_URL = 'https://pure-badlands-1438.herokuapp.com';
+    localforage.setDriver(localforage.WEBSQL);
+
+
+    window.mostachoUserConfig.init()
+      .then(function () {
+        return window.mostachoI18n.init();
+      })
+      .then(function () {
+        return window.mostachoRouteService.init();
+      })
+      .then(function () {
+        document.addEventListener('deviceready', function () {
+          window.mostacho.admobService.init();
+          window.mostacho.analyticsService.init();   
+        });
+      });
+  });
+
+})();
